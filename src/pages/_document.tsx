edeb6094ab4085db31b47f66065792a82bdf5d46
Teerut Srithongdee <>
2023-03-02 15:17:32 +0700

    add hot jar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,5 @@
 import { NextPage } from "next";
-import {
-  Html,
-  Head,
-  NextScript,
-  Main,
-} from "next/document";
+import { Html, Head, NextScript, Main } from "next/document";
 
 interface Props {}
 
@@ -45,7 +40,24 @@ const _document: NextPage<Props> = () => {
           href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         />
-        <script async defer data-website-id="300321f7-6a33-46d1-9135-dd14724da1ee" src="https://umami.csku.in.th/umami.js"></script>
+        <script
+          async
+          defer
+          data-website-id="300321f7-6a33-46d1-9135-dd14724da1ee"
+          src="https://umami.csku.in.th/umami.js"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3390249,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        />
       </Head>
       <body>
         <Main />
