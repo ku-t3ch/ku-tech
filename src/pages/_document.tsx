5bd488b20422c450774bbf5c8299eae6a04c00c7
Teerut Srithongdee <teerutsr@gmail.com>
2023-06-07 17:00:44 +0700

    add umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,14 +14,11 @@ const _document: NextPage<Props> = () => {
           href="https://fonts.gstatic.com"
           crossOrigin=""
         />
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-          rel="stylesheet"
-        /> */}
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <script async src="https://umami-teerut.vercel.app/script.js" data-website-id="1d278cc7-aa50-4fba-932b-853a26ec0197"></script>
       </Head>
       <body>
         <Main />
