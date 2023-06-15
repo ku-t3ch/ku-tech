310b3efd40d1938977e02daadbdb37fdfb5e14e9
nabhansuwanachote <nabhan.au@gmail.com>
2023-06-16 03:11:36 +0900

    feat: change svg button icon

--- src/components/MembersComponent.tsx
+++ src/components/MembersComponent.tsx
@@ -2,7 +2,9 @@ import { NextPage } from "next";
 import AvatarComponent from "@/components/AvatarComponent";
 import { Tag } from "@/interfaces/TagsInterface";
 import { useState } from "react";
-import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
+import Xarrow, { useXarrow } from "react-xarrows";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 interface Member {
   first_name_th: string | null;
@@ -35,7 +37,7 @@ const MembersComponent: NextPage<MemberComponentProps> = (props) => {
     return props.findTag(props.tagName)[0];
   })
   const updateXarrow = useXarrow();
-  const [showChildNode, setShowChildNode] = useState(true);
+  const [showChildNode, setShowChildNode] = useState(false);
 
   const getChildClassName = (parentTagName: string | undefined) => {
     return parentTagName == undefined ? "flex w-full justify-center gap-20 mt-20 content-start" : "flex flex-col mt-10 ml-[14rem] relative bottom-0";
@@ -64,7 +66,7 @@ const MembersComponent: NextPage<MemberComponentProps> = (props) => {
     return (
       <>
         <button type="button" className="text-white bg-stone-600 hover:bg-stone-700 font-medium rounded-full text-sm p-1 text-center inline-flex items-center absolute bottom-1.5" onClick={handleShowChildNode}>
-          <svg aria-hidden="true" className="w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          <FontAwesomeIcon className="w-3 h-3 m-auto" icon={showChildNode ? faMinus : faPlus} />
         </button>
       </>
     )
