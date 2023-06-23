876db13323d9196da8ac68c0ac37c84e85775a2f
Teerut Srithongdee <teerutsr@gmail.com>
2023-06-23 16:45:42 +0700

    add flex wrap reverse

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -41,25 +41,13 @@ const Members: NextPage<Props> = () => {
       {!coreTeamApi.isLoading ? (
         <>
           <div className="mx-auto w-full max-w-[80rem] flex-col gap-3 p-5 md:flex-row md:p-10">
-            {width > 1000 ? (
-              <Xwrapper>
-                <MembersComponent
-                  findName={findName}
-                  findTag={findTag}
-                  tagName="ประธาน"
-                  id="ประธาน"
-                  headTagName="ประธาน"
-                />
-              </Xwrapper>
-            ) : (
-              // Please remove this condition and old organize design after new mobile view is ready
-              <div className="flex flex-col gap-[8rem]">
+          <div className="flex flex-col gap-[8rem]">
                 {countUser("ฝ่ายบริหาร") > 0 && (
                   <div className="flex flex-col items-center gap-3">
                     <Text weight={"bold"} className="prompt" size={"$4xl"}>
                       ฝ่ายบริหาร
                     </Text>
-                    <div className="flex w-full flex-wrap items-center justify-center gap-5">
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
                       {findName("ประธาน")?.map((tag, index) => (
                         <AvatarComponent
                           {...tag}
@@ -68,7 +56,7 @@ const Members: NextPage<Props> = () => {
                         />
                       ))}
                     </div>
-                    <div className="flex w-full flex-wrap items-center justify-center gap-5">
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
                       {findName("รองประธาน")?.map((tag, index) => (
                         <AvatarComponent
                           {...tag}
@@ -98,7 +86,7 @@ const Members: NextPage<Props> = () => {
                     <Text weight={"bold"} className="prompt" size={"$4xl"}>
                       ฝ่ายสารสนเทศ
                     </Text>
-                    <div className="flex w-full flex-wrap items-center justify-center gap-5">
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
                       {findName("ฝ่ายสารสนเทศ")?.map((tag, index) => (
                         <AvatarComponent
                           {...tag}
@@ -115,7 +103,7 @@ const Members: NextPage<Props> = () => {
                       <Text weight={"bold"} className="prompt" size={"$4xl"}>
                         ฝ่ายกิจกรรม
                       </Text>
-                      <div className="flex w-full flex-wrap items-center justify-center gap-5">
+                      <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
                         {findName("ฝ่ายกิจกรรม")?.map((tag, index) => (
                           <AvatarComponent
                             {...tag}
@@ -132,7 +120,7 @@ const Members: NextPage<Props> = () => {
                     <Text weight={"bold"} className="prompt" size={"$4xl"}>
                       ฝ่ายโสตฯ
                     </Text>
-                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
+                    <div className="flex w-full flex-wrap-reverse-reverse items-center justify-center gap-5">
                       {findName("ฝ่ายโสตฯ")?.map((tag, index) => (
                         <AvatarComponent
                           {...tag}
@@ -148,7 +136,7 @@ const Members: NextPage<Props> = () => {
                     <Text weight={"bold"} className="prompt" size={"$4xl"}>
                       เหรัญญิก
                     </Text>
-                    <div className="flex w-full flex-wrap items-center justify-center gap-5">
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
                       {findName("เหรัญญิก")?.map((tag, index) => (
                         <AvatarComponent
                           {...tag}
@@ -164,7 +152,7 @@ const Members: NextPage<Props> = () => {
                     <Text weight={"bold"} className="prompt" size={"$4xl"}>
                       ฝ่ายประชาสัมพันธ์
                     </Text>
-                    <div className="flex w-full flex-wrap items-center justify-center gap-5">
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
                       {findName("ฝ่ายประชาสัมพันธ์")?.map((tag, index) => (
                         <AvatarComponent
                           {...tag}
@@ -180,7 +168,7 @@ const Members: NextPage<Props> = () => {
                     <Text weight={"bold"} className="prompt" size={"$4xl"}>
                       ผู้ร่วมก่อตั้ง
                     </Text>
-                    <div className="flex w-full flex-wrap items-center justify-center gap-5">
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
                       {findName("ผู้ร่วมก่อตั้ง")?.map((tag, index) => (
                         <AvatarComponent
                           {...tag}
@@ -192,7 +180,158 @@ const Members: NextPage<Props> = () => {
                   </div>
                 )}
               </div>
-            )}
+            {/* {width > 1000 ? (
+              <Xwrapper>
+                <MembersComponent
+                  findName={findName}
+                  findTag={findTag}
+                  tagName="ประธาน"
+                  id="ประธาน"
+                  headTagName="ประธาน"
+                />
+              </Xwrapper>
+            ) : (
+              // Please remove this condition and old organize design after new mobile view is ready
+              <div className="flex flex-col gap-[8rem]">
+                {countUser("ฝ่ายบริหาร") > 0 && (
+                  <div className="flex flex-col items-center gap-3">
+                    <Text weight={"bold"} className="prompt" size={"$4xl"}>
+                      ฝ่ายบริหาร
+                    </Text>
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
+                      {findName("ประธาน")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"ประธาน"}
+                        />
+                      ))}
+                    </div>
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
+                      {findName("รองประธาน")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"รองประธาน"}
+                        />
+                      ))}
+                      {findName("เลขานุการ")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"เลขานุการ"}
+                        />
+                      ))}
+                      {findName("รองเลขานุการ")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"รองเลขานุการ"}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
+                {countUser("ฝ่ายสารสนเทศ") > 0 && (
+                  <div className="flex flex-col items-center gap-3">
+                    <Text weight={"bold"} className="prompt" size={"$4xl"}>
+                      ฝ่ายสารสนเทศ
+                    </Text>
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
+                      {findName("ฝ่ายสารสนเทศ")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"ฝ่ายสารสนเทศ"}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
+                {countUser("ฝ่ายกิจกรรม") > 0 && (
+                  <div className="flex flex-col items-center gap-3">
+                    <div className="flex flex-col items-center gap-3">
+                      <Text weight={"bold"} className="prompt" size={"$4xl"}>
+                        ฝ่ายกิจกรรม
+                      </Text>
+                      <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
+                        {findName("ฝ่ายกิจกรรม")?.map((tag, index) => (
+                          <AvatarComponent
+                            {...tag}
+                            key={index}
+                            position={"ฝ่ายกิจกรรม"}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                )}
+                {countUser("ฝ่ายโสตฯ") > 0 && (
+                  <div className="flex flex-col items-center gap-3">
+                    <Text weight={"bold"} className="prompt" size={"$4xl"}>
+                      ฝ่ายโสตฯ
+                    </Text>
+                    <div className="flex w-full flex-wrap-reverse-reverse items-center justify-center gap-5">
+                      {findName("ฝ่ายโสตฯ")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"ฝ่ายโสตฯ"}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
+                {countUser("เหรัญญิก") > 0 && (
+                  <div className="flex flex-col items-center gap-3">
+                    <Text weight={"bold"} className="prompt" size={"$4xl"}>
+                      เหรัญญิก
+                    </Text>
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
+                      {findName("เหรัญญิก")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"เหรัญญิก"}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
+                {countUser("ฝ่ายประชาสัมพันธ์") > 0 && (
+                  <div className="flex flex-col items-center gap-3">
+                    <Text weight={"bold"} className="prompt" size={"$4xl"}>
+                      ฝ่ายประชาสัมพันธ์
+                    </Text>
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
+                      {findName("ฝ่ายประชาสัมพันธ์")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"ฝ่ายประชาสัมพันธ์"}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
+                {countUser("ผู้ร่วมก่อตั้ง") > 0 && (
+                  <div className="flex flex-col items-center gap-3">
+                    <Text weight={"bold"} className="prompt" size={"$4xl"}>
+                      ผู้ร่วมก่อตั้ง
+                    </Text>
+                    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-5">
+                      {findName("ผู้ร่วมก่อตั้ง")?.map((tag, index) => (
+                        <AvatarComponent
+                          {...tag}
+                          key={index}
+                          position={"ผู้ร่วมก่อตั้ง"}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </div>
+            )} */}
           </div>
         </>
       ) : (
