f9a8d874c106163ae6a128f16cace233ccb737f9
Teerut Srithongdee <teerutsr@gmail.com>
2023-06-24 15:15:53 +0700

    fix next/image

--- src/components/home/ImageCarousel.tsx
+++ src/components/home/ImageCarousel.tsx
@@ -18,7 +18,9 @@ const ImageCarousel: NextPage<Props> = () => {
         <SplideSlide className="relative overflow-hidden rounded-2xl" key={i}>
           <Image
             className="h-[30rem] w-full object-cover"
-            width={1080} height={1920}
+            width={0}
+            height={0}
+            sizes="100vw"
             src={`/activities/${i + 1}.jpg`}
             alt={`Image ${i + 1}`}
           />
@@ -26,9 +28,12 @@ const ImageCarousel: NextPage<Props> = () => {
             className={clsx(
               "bg absolute bottom-0 right-0 left-0 z-30 flex flex-col items-center justify-center py-7 px-3 font-bold",
               css`
-                background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.81) 0%, rgba(0, 0, 0, 0.49) 55%, rgba(0, 0, 0, 0) 100%);
-
-                /* border-radius: 0 0 1rem 0; */
+                background-image: linear-gradient(
+                  0deg,
+                  rgba(0, 0, 0, 0.81) 0%,
+                  rgba(0, 0, 0, 0.49) 55%,
+                  rgba(0, 0, 0, 0) 100%
+                );
               `
             )}
           >
