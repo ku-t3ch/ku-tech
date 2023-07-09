0f7b888e1a01c7a8ade3a4a1d72e4b6e6b0d83e0
Teerut Srithongdee <teerutsr@gmail.com>
2023-07-09 16:39:56 +0700

    [+] add page document downlaod

---
+++ src/pages/documents-download.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const DocumentsDownload: NextPage<Props> = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default DocumentsDownload
