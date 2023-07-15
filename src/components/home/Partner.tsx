97c00935ff9ed2965731524fabb94990b98cd36f
Teerut Srithongdee <teerutsr@gmail.com>
2023-07-16 02:25:53 +0700

    [+] add partner page

---
+++ src/components/home/Partner.tsx
@@ -0,0 +1,66 @@
+import { Avatar, Text } from "@nextui-org/react";
+import { Tooltip } from "antd";
+import { NextPage } from "next";
+import { useMediaQuery } from "usehooks-ts";
+
+interface Props {}
+
+const PartnerData = [
+  {
+    name: "สภาผู้แทนนิสิต มก",
+    image: "https://s3.tech.nisit.ku.ac.th/assets/partner/สภาผู้แทนนิสิต.png",
+    link: "https://www.facebook.com/kusc.bk",
+  },
+  {
+    name: "องค์การบริหาร องค์การนิสิต มหาวิทยาลัยเกษตรศาสตร์ บางเขน",
+    image: "https://s3.tech.nisit.ku.ac.th/assets/partner/อบก.png",
+    link: "https://www.facebook.com/kusab.bk/",
+  },
+  {
+    name: "GDSC KU",
+    image: "https://s3.tech.nisit.ku.ac.th/assets/partner/GDSCKU.png",
+    link: "https://www.facebook.com/gdsc.ku/",
+  },
+  {
+    name: "KU Blockchain",
+    image: "https://s3.tech.nisit.ku.ac.th/assets/partner/KUBlockchain.jpg",
+    link: "https://www.facebook.com/KUBCS/",
+  },
+  {
+    name: "KU Startup",
+    image: "https://s3.tech.nisit.ku.ac.th/assets/partner/KUStartup.jpg",
+    link: "https://www.facebook.com/Kustartup",
+  },
+];
+
+const Partner: NextPage<Props> = () => {
+  const isMobile = useMediaQuery("(max-width: 640px)");
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center gap-5">
+        <Text b size={"$4xl"} className="">
+          พันธมิตร (Partner)
+        </Text>
+        <div className="flex flex-wrap justify-center gap-5 text-white">
+          {PartnerData.map((partner, index) => (
+            <Tooltip title={partner.name} key={index}>
+              <a className="flex flex-col" href={partner.link} target="_bank">
+                <Avatar
+                  css={{ width: isMobile ? "5rem" : "6rem", height: isMobile ? "5rem" : "6rem" }}
+                  src={partner.image}
+                  color="gradient"
+                  alt={partner.name}
+                  zoomed
+                  bordered
+                />
+              </a>
+            </Tooltip>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Partner;
