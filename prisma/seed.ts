cdd912dca02dd8494110559b551180f72855b79c
ekkamon <ekkb.contact@gmail.com>
2023-07-15 21:14:40 +0700

    update: schema prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,23 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.party.createMany({
+    data: [
+      {
+        name: 'ฝ่ายบริหาร'
+      },
+      {
+        name: 'ฝ่ายสารสนเทศ'
+      },
+      {
+        name: 'ฝ่ายกิจกรรม'
+      },
+      {
+        name: 'ฝ่ายประชาสัมพันธ์'
+      }
+    ]
+  })
+
   await prisma.budget.create({
     data: {
       name: "งบประมาณประจำปี 2566",
@@ -12,11 +29,29 @@ async function main() {
         create: [
           {
             name: 'TechCamp #1',
-            amount: 50_000
+            spendingUse: {
+              create: [
+                {
+                  name: 'ค่าเดินทาง',
+                  amount: 5_000,
+                },
+                {
+                  name: 'ค่าอาหารสำหรับ Staff',
+                  amount: 3_000,
+                }
+              ]
+            }
           },
           {
             name: 'FirstMeet #1',
-            amount: 10_000
+            spendingUse: {
+              create: [
+                {
+                  name: 'ค่าเช่า True Lab',
+                  amount: 10_000,
+                }
+              ]
+            }
           }
         ]
       }
