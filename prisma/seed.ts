6852de31ecc1c8efbe1b69fbcc73af8952247def
Ekkamon Munsri <34152283+ekkamon@users.noreply.github.com>
2023-07-24 11:47:33 +0700

    update: prisma schema & prisma seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,57 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
-async function main() {
-  const budget1 = await prisma.budget.create({
+
+
+const main = async () => {
+  await prisma.budget.create({
     data: {
-      name: "Budget 1",
-      amount: 120_000,
+      name: "โครงการปีการศึกษา 2566",
+      amount: 7_000_000,
+      received_amount: {
+        create: [
+          {
+            name: 'งบกิจกรรมนิสิต',
+            amount: 20_000
+          },
+          {
+            name: 'ได้รับจัดสรร',
+            amount: 120_000
+          },
+          {
+            name: 'ผู้สนับสนุน',
+            amount: 20_000
+          }
+        ]
+      },
+      projectUse: {
+        create: [
+          {
+            name: 'โครงการ Techcamp ครั้งที่ 1',
+            amount: 9_000,
+            spendingUse: {
+              create: [
+                {
+                  name: 'ค่าจ้างเหมารถตู้',
+                  amount: 2700
+                },
+                {
+                  name: 'กระดาษ A4',
+                  amount: 140
+                },
+                {
+                  name: 'กระดาษ 300 G',
+                  amount: 300
+                }
+              ]
+            }
+          },
+          {
+            name: 'โครงการ FirstMeet',
+            amount: 0
+          }
+        ]
+      }
     },
   });
 }
