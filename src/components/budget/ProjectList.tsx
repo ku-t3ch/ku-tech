ff945013f494c47a21ca6d6af65e047eb3f6d9c7
Ekkamon Munsri <34152283+ekkamon@users.noreply.github.com>
2023-07-29 23:48:45 +0700

    update: add table colums icon

--- src/components/budget/ProjectList.tsx
+++ src/components/budget/ProjectList.tsx
@@ -1,39 +1,46 @@
+import tw from "tailwind-styled-components";
+
 import { NextPage } from "next";
 import { FileIcon } from "react-file-icon";
 
-import tw from "tailwind-styled-components";
+import { Icon } from "@iconify/react";
 import { TBody, Table, Thead } from "./Table";
 
 interface Project {
   name: string | null;
   amount: number | null;
+  document_url: string | null;
 }
 
 interface Props {
   data?: Project[];
 }
 
-const ProjectList: NextPage<Props> = ({ data = [] }) => {
-  const columns = [
-    {
-      key: "id",
-      label: "ลำดับ",
-    },
-    {
-      key: "project-name",
-      label: "ชื่อโครงการ",
-      alignLeft: "start",
-    },
-    {
-      key: "amount",
-      label: "ได้รับจำนวน",
-    },
-    {
-      key: "description",
-      label: "รายละเอียด",
-    },
-  ];
+const columns = [
+  {
+    key: "id",
+    label: "ลำดับ",
+    icon: "ant-design:number-outlined",
+  },
+  {
+    key: "project-name",
+    label: "ชื่อโครงการ",
+    icon: "ant-design:project-outlined",
+    align: "start",
+  },
+  {
+    key: "amount",
+    label: "ได้รับจำนวน",
+    icon: "dashicons:money-alt",
+  },
+  {
+    key: "description",
+    label: "รายละเอียด",
+    icon: "solar:document-text-outline",
+  },
+];
 
+const ProjectList: NextPage<Props> = ({ data = [] }) => {
   return (
     <Table.Container className="max-h-[18rem]">
       <Table.Main>
@@ -41,13 +48,16 @@ const ProjectList: NextPage<Props> = ({ data = [] }) => {
           <Thead.Tr>
             {columns.map((v, idx) => {
               return (
-                <Thead.Th
-                  key={idx}
-                  style={{
-                    textAlign: v.alignLeft ? "start" : "center",
-                  }}
-                >
-                  {v.label}
+                <Thead.Th key={idx}>
+                  <div
+                    className="flex items-center justify-center gap-1"
+                    style={{
+                      justifyContent: v.align ?? "center",
+                    }}
+                  >
+                    <Icon icon={v.icon ?? ""} />
+                    <span className="whitespace-nowrap">{v.label}</span>
+                  </div>
                 </Thead.Th>
               );
             })}
@@ -60,10 +70,17 @@ const ProjectList: NextPage<Props> = ({ data = [] }) => {
               <TBody.Tr key={idx}>
                 <TBody.Td className="p-[1rem]">{idx + 1}</TBody.Td>
                 <TBody.Td className="text-start">{v.name}</TBody.Td>
-                <TBody.Td>{v.amount?.toLocaleString()}</TBody.Td>
+                <TBody.Td>{v.amount?.toLocaleString()} THB</TBody.Td>
                 <TBody.Td>
                   <div className="flex justify-center">
-                    <div role="button" className="w-[1rem]">
+                    <div
+                      role="button"
+                      className="w-[1rem]"
+                      onClick={() => {
+                        if (!v?.document_url) return;
+                        window.open(v.document_url, "_blank", "noreferrer");
+                      }}
+                    >
                       <FileIcon type="document" extension="pdf" color="aliceblue" />
                     </div>
                   </div>
@@ -76,13 +93,5 @@ const ProjectList: NextPage<Props> = ({ data = [] }) => {
     </Table.Container>
   );
 };
-
-const Title = tw.div`
-  py-[.5rem]
-  text-center
-  text-[1.15rem]
-  font-bold
-  tracking-wide
-`;
 
 export default ProjectList;
