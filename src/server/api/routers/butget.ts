841277aac6b20a41e386cbe804e7c7de9c23974f
Ekkamon Munsri <34152283+ekkamon@users.noreply.github.com>
2023-09-23 23:28:04 +0700

    update!: budget api

--- src/server/api/routers/butget.ts
+++ src/server/api/routers/butget.ts
@@ -1,4 +1,4 @@
-
+import { z } from "zod";
 import {
   createTRPCRouter,
   publicProcedure,
@@ -9,6 +9,7 @@ export const budgetRouter = createTRPCRouter({
   getBudget: publicProcedure.query(async () => {
     return await prisma.budget.findFirst({
       select: {
+        id: true,
         name: true,
         amount: true,
         received_amount: {
@@ -19,18 +20,9 @@ export const budgetRouter = createTRPCRouter({
         },
         projectUse: {
           select: {
-            id: true,
             name: true,
             amount: true,
-            document_url: true,
-            start_date: true,
-            ended_date: true,
-            spendingUse: {
-              select: {
-                name: true,
-                amount: true
-              }
-            }
+            document_url: true
           }
         }
       }
@@ -38,10 +30,38 @@ export const budgetRouter = createTRPCRouter({
   }),
   getSponsor: publicProcedure.query(async () => {
     return await prisma.sponsor.findMany({
-  
       select: {
         brand_logo: true,
         brand_name: true,
+      }
+    })
+  }),
+  getProjects: publicProcedure.input(z.object({
+    budgetId: z.string().nullish(),
+    orderBy: z.enum(['desc', 'asc']),
+  })).query(async ({ input }) => {
+    return await prisma.project.findMany({
+      orderBy: {
+        start_date: input?.orderBy ?? 'desc',
+      },
+      where: {
+        budgetUse: {
+          some: {
+            id: input.budgetId ?? ""
+          }
+        }
+      },
+      select: {
+        name: true,
+        amount: true,
+        start_date: true,
+        ended_date: true,
+        spendingUse: {
+          select: {
+            name: true,
+            amount: true
+          }
+        }
       }
     })
   })
