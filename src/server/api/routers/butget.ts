cabe4eb2b0b64ab9472af944d04bcca80ec3323d
Ekkamon Munsri <34152283+ekkamon@users.noreply.github.com>
2023-09-24 01:36:51 +0700

    fix(api): get last budget

--- src/server/api/routers/butget.ts
+++ src/server/api/routers/butget.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
-import {
-  createTRPCRouter,
-  publicProcedure,
-} from "@/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { prisma } from "@/server/db";
 
 export const budgetRouter = createTRPCRouter({
   getBudget: publicProcedure.query(async () => {
     return await prisma.budget.findFirst({
+      orderBy: {
+        id: "desc",
+      },
+      take: 1,
       select: {
         id: true,
         name: true,
@@ -15,54 +16,58 @@ export const budgetRouter = createTRPCRouter({
         received_amount: {
           select: {
             name: true,
-            amount: true
-          }
+            amount: true,
+          },
         },
         projectUse: {
           select: {
             name: true,
             amount: true,
-            document_url: true
-          }
-        }
-      }
-    })
+            document_url: true,
+          },
+        },
+      },
+    });
   }),
   getSponsor: publicProcedure.query(async () => {
     return await prisma.sponsor.findMany({
       select: {
         brand_logo: true,
         brand_name: true,
-      }
-    })
+      },
+    });
   }),
-  getProjects: publicProcedure.input(z.object({
-    budgetId: z.string().nullish(),
-    orderBy: z.enum(['desc', 'asc']),
-  })).query(async ({ input }) => {
-    return await prisma.project.findMany({
-      orderBy: {
-        start_date: input?.orderBy ?? 'desc',
-      },
-      where: {
-        budgetUse: {
-          some: {
-            id: input.budgetId ?? ""
-          }
-        }
-      },
-      select: {
-        name: true,
-        amount: true,
-        start_date: true,
-        ended_date: true,
-        spendingUse: {
-          select: {
-            name: true,
-            amount: true
-          }
-        }
-      }
-    })
-  })
+  getProjects: publicProcedure
+    .input(
+      z.object({
+        budgetId: z.string().nullish(),
+        orderBy: z.enum(["desc", "asc"]),
+      })
+    )
+    .query(async ({ input }) => {
+      return await prisma.project.findMany({
+        orderBy: {
+          start_date: input?.orderBy ?? "desc",
+        },
+        where: {
+          budgetUse: {
+            some: {
+              id: input.budgetId ?? "",
+            },
+          },
+        },
+        select: {
+          name: true,
+          amount: true,
+          start_date: true,
+          ended_date: true,
+          spendingUse: {
+            select: {
+              name: true,
+              amount: true,
+            },
+          },
+        },
+      });
+    }),
 });
