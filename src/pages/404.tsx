b3f6cec961550399759a5d5f32cf5599064705da
Natdanai Pinaves <jgogo012@gmail.com>
2025-07-11 14:51:39 +0700

    Fix: Remove Animation in 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,36 +1,22 @@
-import Lottie from 'lottie-react';
-import animationData from "@/assets/lottiefiles/lf20_u1xuufn3.json";
-
-import { NextPage } from "next";
-import { Text } from "@nextui-org/react";
+import React from 'react';
 
 interface Props {}
 
-const _404: NextPage<Props> = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
+const Custom404: React.FC<Props> = () => {
   return (
-    <div className="mx-auto max-w-[73rem] flex-col gap-10 p-5 md:flex-row md:p-10">
+    <div className="min-h-screen flex items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-5">
-        <Lottie options={defaultOptions} height={400} width={400} />
         <div className="flex flex-col items-center">
-          <Text className="prompt font-bold" size={"$7xl"}>
+          <h1 className="text-7xl font-bold text-primary">
             404
-          </Text>
-          <Text className="prompt" size={"$2xl"}>
+          </h1>
+          <h2 className="text-2xl">
             Page Not Found
-          </Text>
+          </h2>
         </div>
       </div>
     </div>
   );
 };
 
-export default _404;
+export default Custom404;
